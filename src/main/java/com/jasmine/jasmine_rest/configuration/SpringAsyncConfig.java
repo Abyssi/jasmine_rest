5be0282ec828e4a9453fbe3b4450c189b4f47830
sashacodes <fr.ottaviano@gmail.com>
2018-09-18 14:36:21 +0200

    Spring Async ThreadPool some values set. Async and EnableAsync annotations missing.

---
+++ src/main/java/com/jasmine/jasmine_rest/configuration/SpringAsyncConfig.java
@@ -0,0 +1,24 @@
+package com.jasmine.jasmine_rest.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(7);
+        executor.setMaxPoolSize(42);
+        executor.setQueueCapacity(11);
+        executor.setThreadNamePrefix("threadPoolExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
